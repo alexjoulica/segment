b9fc80c9def9a561a5b2a4c967bbdde1b279efe9
alexjoulica <139750354+alexjoulica@users.noreply.github.com>
2023-07-17 11:52:21 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,8 @@
 <?php  
 $text=$_POST["input"];//receiving name field value in $text variable  
-  
+
+require_once("/path/to/analytics-php/lib/Segment.php");
+use Segment\Segment;
+
 echo "You Entered: $text";
 ?>  
