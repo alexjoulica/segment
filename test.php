820ef00024706c63310ba5e495fec9998769893a
alexjoulica <139750354+alexjoulica@users.noreply.github.com>
2023-07-17 12:20:20 +0100

    Delete test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php  
-$text=$_POST["input"];//receiving name field value in $text variable  
-
-require_once("/path/to/analytics-php/lib/Segment.php");
-use Segment\Segment;
-
-echo "You Entered: $text";
-?>  
