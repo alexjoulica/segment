41b76a3c86151690bf5c91223dd644b53df9c39d
alexjoulica <139750354+alexjoulica@users.noreply.github.com>
2023-08-31 11:19:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let urlString = window.location.search;
 let urlParams = new URLSearchParams(urlString);
 let pageType = urlParams.get('page_type');
 if (!localStorage.getItem("page_type")) {
+     localStorage.removeItem("page_type");
      localStorage.setItem("page_type", pageType);
 }
 let json = "";
